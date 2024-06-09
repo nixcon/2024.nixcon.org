a5ac1a6de035df48f86d14dab94d550f66f66b54
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-06-09 15:02:35 +0200

    Simplify flake and include other arch

--- flake.nix
+++ flake.nix
@@ -1,13 +1,37 @@
 {
   description = "Nixcon 2024 Website";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
 
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
-  };
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system: {
+        nixcon-2024-website = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {
+          pkgs = nixpkgsFor.${system};
+          inherit version;
+        };
+      });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.nixcon-2024-website);
+    };
 }
